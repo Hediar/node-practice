ffad5c297907ff9ed47251212416f5b1262ef382
seryeong <srlimvp@gmail.com>
2024-11-17 23:06:02 +0900

    ejs + controller

--- app.js
+++ app.js
@@ -1,25 +1,24 @@
+const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
-const path = require('path');
-const expressHbs = require('express-handlebars');
+
+const errorController = require('./controllers/error');
 
 const app = express();
 
-app.engine('hbs', expressHbs({layoutsDir: 'views/layouts/', defaultLayout: 'main-layout', extname: 'hbs'})) // 설정한 이름 동일하게 사용하기
-app.set('view engine', 'hbs'); // key로 express와 연결 (pug => hbs)
-app.set('views', 'views'); //기본값이 views이지만 다른 이름으로 저장 가능
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
-const adminData = require('./routes/admin');
+const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminData.routes); // 객체로 받기
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-    res.status(404).render('404', {pageTitle: 'Page Not Found'});
-})
+app.use(errorController.get404);
 
 app.listen(3000);
--- routes/shop.js
+++ routes/shop.js
@@ -1,15 +1,11 @@
-const express = require('express');
 const path = require('path');
 
-const rootDir = require('../util/path');
-const adminData = require('./admin');
+const express = require('express');
+
+const productsController = require('../controllers/products');
 
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-  const products = adminData.products;
-  res.render('shop', {prods: products, pageTitle: 'Shop', path: '/', hasProducts: products.length > 0, activeShop: true, productCSS: true, layout: false});
-    // 두번째 변수는 템플릿으로 데이터 전달
-});
+router.get('/', productsController.getProducts);
 
 module.exports = router;
