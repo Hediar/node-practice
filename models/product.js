713a6e3f42d33031d783f84b82f3b978e6ecdf29
seryeong <srlimvp@gmail.com>
2024-11-24 22:41:32 +0900

    model refactoring

--- models/product.js
+++ models/product.js
@@ -2,32 +2,33 @@ const products = [];
 // 파일에 저장하기
 const fs = require('fs');
 const path = require('path');
+const p = path.join(path.dirname(process.mainModule.filename), 'data', 'products.json');
+
+const getProductsFromFile = (cb) => { // fetchAll에서 하는일 옮김
+    fs.readFile(p, (err, fileContent) => {
+        if (err) {
+            cb([]);
+        } else{
+            cb(JSON.parse(fileContent));
+        }
+    })
+}
 
 module.exports = class Product{
     constructor(t){
         this.title = t;
     }
     save(){
-        products.push(this);
-        const p = path.join(path.dirname(process.mainModule.filename), 'data', 'products.json');
-        fs.readFile(p, (err, fileContent) => {
-            let products =[];
-            if (!err) {
-                products = JSON.parse(fileContent);
-            }
+        getProductsFromFile(products => {
             products.push(this);
-            fs.writeFile(p, JSON.stringify(products), (err) => {console.log(err);});
-        }); // 전체 파일 읽기
+            fs.writeFile(p, JSON.stringify(products), err => {
+                console.log(err);
+            });
+        });
     }
 
     static fetchAll(cb) { // 제품 반환
-        const p = path.join(path.dirname(process.mainModule.filename), 'data', 'products.json');
-        fs.readFile(p, (err, fileContent) => {
-            if (err) {
-                cb([]);
-            } 
-            cb(JSON.parse(fileContent));
-        })
+      getProductsFromFile(cb);
     }
     /**
      * fetchAll -> 함수 실행 -> 제품 나옴 -> 해당 제품을 통해 응답 렌더링
