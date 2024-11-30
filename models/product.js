5da6e988de1358b004b102e74f6882c9b954bfe3
seryeong <srlimvp@gmail.com>
2024-11-30 22:10:25 +0900

    동적 라우팅 전, 정리 작업

--- models/product.js
+++ models/product.js
@@ -15,9 +15,12 @@ const getProductsFromFile = (cb) => { // fetchAll에서 하는일 옮김
 }
 
 module.exports = class Product{
-    constructor(t){
-        this.title = t;
-    }
+    constructor(title, imageUrl, description, price) {
+        this.title = title;
+        this.imageUrl = imageUrl;
+        this.description = description;
+        this.price = price;
+      }
     save(){
         getProductsFromFile(products => {
             products.push(this);
