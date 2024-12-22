64722576130bbf82d2f740ac0b096974fb36d1cc
seryeong <srlimvp@gmail.com>
2024-12-22 23:43:40 +0900

    미들웨어 및 postOrder error 수정

--- middleware/is-auth.js
+++ middleware/is-auth.js
@@ -1,6 +1,24 @@
+const User = require('../models/user');
+
 module.exports = (req, res, next) => {
-    if (!req.session.isLoggedIn) {
-        return res.redirect('/login');
+  if (!req.session.isLoggedIn) {
+    return res.redirect('/login');
+  }
+
+  if (!req.session.user) {
+    return res.redirect('/login');
+  }
+
+  User.findById(req.session.user._id)
+    .then(user => {
+      if (!user) {
+        return res.redirect('/login'); 
       }
-      next();
-}
+      req.user = user; 
+      next(); 
+    })
+    .catch(err => {
+      console.error('Error fetching user:', err);
+      res.redirect('/login');
+    });
+};
